80ea5fe92e879e89b9b174301e477c1680de5dd6
hlc <hlc157248yx@163.com>
2016-12-31 12:25:14 +0800

    增加商品内容页样式布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -10,7 +10,34 @@
           </li>
         </ul>
       </div>
-      <div class="foots-wrapper"></div>
+      <div class="foots-wrapper">
+        <ul>
+          <li v-for="item in goods" class="foods-list">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="food in item.foods" class="food-item border-1px">
+                <div class="foodicon">
+                  <img width="57" height="57" :src="food.icon" />
+                </div>
+
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}</span>
+                  </div>
+                  <div class="price">
+                    <span class="new">¥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 
@@ -32,6 +59,10 @@
             response = response.body;
             if (response.erron === 0) {
               this.goods = response.data;
+
+              for(let item in this.goods){
+                console.log(this.goods[item].name);
+              }
             }
           })
         },
@@ -68,6 +99,54 @@
           font-size 12px
     .foots-wrapper
       flex:1
+      .title
+        padding-left 14px
+        height: 26px
+        line-height 26px
+        border-left 2px solid #d9dde1
+        font-size 12px
+        color rgb(147,153,159)
+        background #F3F5F7
+      .food-item
+        display flex
+        margin 18px
+        padding-bottom 18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom 0
+        .foodicon
+          flex: 0 0 57px
+          margin-right 10px
+        .content
+          flex 1
+          .name
+            font-size 17px
+            margin:2px 0 8px 0
+            height 17px
+            line-height 17px
+            color:rgb(7,17,27)
+          .desc
+            margin-bottom 8px
+            line-height 10px
+            font-size 10px
+            color rgb(147,153,159)
+          .extra
+            margin-bottom 8px
+            line-height 10px
+            font-size 10px
+            color rgb(147,153,159)
+            .count
+              margin-right 12px
+          .price
 
-
+            line-height 24px
+            .new
+              font-size 14px
+              font-weight 700
+              margin-right 8px
+              color rgb(240,20,20)
+            .old
+              text-decoration dashed
+              color rgb(147,153,159)
 </style>
